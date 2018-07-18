156eeec006e5e716abababac50fef2fdf471d9e0
Nunky Lado <puatachok@gmail.com>
2018-07-18 18:20:44 +0300

    Update

--- app/src/main/java/com/example/lado/mytaskmanager/MainActivity.java
+++ app/src/main/java/com/example/lado/mytaskmanager/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.lado.mytaskmanager;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,14 +13,16 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    int id_To_Update = 0;
     EditText input;
     String task;
 
@@ -42,22 +43,23 @@ public class MainActivity extends AppCompatActivity {
         //alertDialog.setMessage("What do you want to do?");
 
         final DBHelper dbHelper = new DBHelper(MainActivity.this);
-        input = new EditText(MainActivity.this);
 
-        final TextView textView = (TextView) findViewById(R.id.TODO);
-        task = String.valueOf(input.getText());
+        final ListView textView = (ListView) findViewById(R.id.TODO);
 
-//        for(int ii = 0; ii<dbHelper.getAllCotacts().size(); ii++) {
-//            textView.setMovementMethod(new ScrollingMovementMethod());
-//            textView.setText(dbHelper.getAllCotacts().get(ii));
-//        }
-
+        //List of tasks
+        List<String> list = new ArrayList<>();
         StringBuilder jokeStringBuilder = new StringBuilder();
         for (String s : dbHelper.getAllCotacts()) {
             jokeStringBuilder.append(s + "\n");
+            list.add(s);
         }
-        textView.setMovementMethod(new ScrollingMovementMethod());
-        textView.setText(jokeStringBuilder.toString());
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                list);
+
+        textView.setAdapter(arrayAdapter);
 
         //make button clickable
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -68,6 +70,9 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "New Task", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
 
+                input = new EditText(MainActivity.this);
+                Log.e("INPUT", "INPUT IS " + input.toString());
+
                 //Setting Dialog Message, and Dialog Title, also add edittext in alertDialog and show everything
                 AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
                 alertDialog.setTitle("Add new Task").setMessage("What do you want to do?").setView(input)
@@ -75,11 +80,20 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
 
+                        Log.e("Hello", "Hello");
+                        task = input.getText().toString();
+                        Log.e("TASK", "TASK IS " + task);
+
                         if(dbHelper.insertTask(task)) {
                             Toast.makeText(MainActivity.this, "Done", Toast.LENGTH_SHORT).show();
                             Log.d("ADDING", "ADDING NEW TASK " + task);
                             Log.d("ALLCONTACTS", "ALL " + dbHelper.getAllCotacts());
                         }
+                        if(dbHelper.updateNote(task)) {
+                            Toast.makeText(MainActivity.this, "Updated", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(MainActivity.this, MainActivity.class));
+                        }
+
                     }
                 }).setNegativeButton("Cancel", null).create().show();
             }
